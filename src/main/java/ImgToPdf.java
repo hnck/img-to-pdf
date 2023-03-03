f12cfa5eb9b873aa5ee4a196a4cb0d50ebefc750
hnck <schrottmailing@googlemail.com>
2023-03-03 23:40:47 +0100

    Updated dependencies and improved implementation

--- src/main/java/ImgToPdf.java
+++ src/main/java/ImgToPdf.java
@@ -3,51 +3,46 @@ import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Image;
 import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.PdfWriter;
-import org.apache.log4j.Logger;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FilenameUtils;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
+@Slf4j
 public class ImgToPdf {
 
-    private static org.apache.log4j.Logger LOG = Logger.getLogger(ImgToPdf.class);
-
     public static void main(String[] args) {
-        new ImgToPdf(args[0], args[1]);
+        new ImgToPdf(Paths.get(args[0]), Paths.get(args[1]));
     }
 
-    public ImgToPdf(final String inputFolder, final String outputFolder) {
-        try {
-            Files.list(Paths.get(inputFolder)).forEach(file -> {
-                if(!file.toFile().isDirectory()) {
-                    final String fileName = file.getFileName().toString();
-                    final String targetFile = fileName.substring(0, fileName.length() - 3) + "pdf";
-                    convert(file.toString(), outputFolder + targetFile);
-                }
-            });
+    public ImgToPdf(final Path inputFolder, final Path outputFolder) {
+        try (final Stream<Path> files = Files.list(inputFolder)) {
+            files.filter(Files::isRegularFile)
+                    .forEach(file -> {
+                        final String output = Paths.get(outputFolder.toString(), FilenameUtils.getBaseName(file.getFileName().toString()) + ".pdf").toString();
+                        try (final FileOutputStream fos = new FileOutputStream(output)) {
+                            final BufferedImage bufferedImage = ImageIO.read(file.toFile());
+                            final Rectangle rectangle = new Rectangle(bufferedImage.getWidth(), bufferedImage.getHeight());
+                            final Document document = new Document(rectangle, 0, 0, 0, 0);
+                            final PdfWriter writer = PdfWriter.getInstance(document, fos);
+                            writer.open();
+                            document.open();
+                            document.add(Image.getInstance(file.toString()));
+                            document.close();
+                            writer.close();
+                        } catch (IOException | DocumentException e) {
+                            log.error("Error", e);
+                        }
+                    });
         } catch (IOException e) {
-            LOG.error(e);
-        }
-    }
-
-    private void convert(final String input, final String output) {
-        try (final FileOutputStream fos = new FileOutputStream(output)) {
-            final BufferedImage bufferedImage = ImageIO.read(new File(input));
-            final Document document = new Document(new Rectangle(bufferedImage.getWidth(), bufferedImage.getHeight()),
-                    0, 0, 0, 0);
-            final PdfWriter writer = PdfWriter.getInstance(document, fos);
-            writer.open();
-            document.open();
-            document.add(Image.getInstance(input));
-            document.close();
-            writer.close();
-        } catch (IOException | DocumentException e) {
-            LOG.error(e);
+            log.error("Error", e);
         }
     }
 }
